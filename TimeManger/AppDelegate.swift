78a933c5149858dc9b7054d1b0551fd083747850
FinchFeng <329650437@qq.com>
2019-04-24 20:43:16 +0800

    计时开始

--- TimeManger/AppDelegate.swift
+++ TimeManger/AppDelegate.swift
@@ -5,14 +5,16 @@
 //  Created by 冯奕琦 on 2019/4/24.
 //  Copyright © 2019 冯奕琦. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var bgTask:UIBackgroundTaskIdentifier!
+    var andioPlayer:AVAudioPlayer?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -25,20 +27,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let app = UIApplication.shared
+        self.bgTask = app.beginBackgroundTask(expirationHandler: {
+            app.endBackgroundTask(self.bgTask)
+            self.bgTask = UIBackgroundTaskIdentifier.invalid
+        })
+        Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(applyForMoreTime), userInfo: nil, repeats: true)
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    
+    @objc func applyForMoreTime(){
+        if UIApplication.shared.backgroundTimeRemaining < 3010 {//时间小于一定数目
+            //播放一次
+            let url = Bundle.main.url(forResource: "1", withExtension: "wav")!
+            let item = AVPlayerItem(url: url)
+            do {
+                try AVAudioSession.sharedInstance().setCategory(
+                    AVAudioSession.Category.playback,//这个模式锁屏和静音模式下可以继续播放
+                    mode: .default,
+                    options: [AVAudioSession.CategoryOptions.mixWithOthers])
+            } catch {
+                print("Failed to set audio session category.  Error: \(error)")
+            }
+            let player = AVQueuePlayer(playerItem: item)
+            player.play()
+            //重新设定一次bgTask
+            UIApplication.shared.endBackgroundTask(self.bgTask)
+            self.bgTask = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+                UIApplication.shared.endBackgroundTask(self.bgTask)
+                 self.bgTask = UIBackgroundTaskIdentifier.invalid
+            })
+            
+        }
     }
 
 
--- TimeManger/ViewController.swift
+++ TimeManger/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer:Timer?
+    var second = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBAction func clocking(){
+        timer?.invalidate()
+        second = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            self.second += 1
+            print(self.second)
+            self.timeLabel.text = " \(self.second/3600):\((self.second/60)%60):\(self.second%60)"
+        })
+    }
 
 }
 
