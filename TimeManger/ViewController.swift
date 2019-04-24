6cc4dd271340fe04fab9752a0fbd45f4e38d0363
FinchFeng <329650437@qq.com>
2019-04-24 19:28:28 +0800

    Initial Commit

---
+++ TimeManger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeManger
+//
+//  Created by 冯奕琦 on 2019/4/24.
+//  Copyright © 2019 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
